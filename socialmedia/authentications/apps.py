4c823fa92361ca4839d6d936e6fce2a2f366baf1
Adefolalu Adegboyega <akinolaay056@gmail.com>
2023-05-20 10:15:47 +0100

    Code rearranged

--- socialmedia/authentications/apps.py
+++ socialmedia/authentications/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationConfig(AppConfig):
+class AuthenticationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name = 'authentications'
